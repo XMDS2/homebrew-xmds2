597c3227dd17c067866a48d7ed531c490e68941b
MattiasJohnsson <2577579+MattiasJohnsson@users.noreply.github.com>
2019-05-06 17:50:31 +1000

    Add libraries required to pass testsuite
    Included python 3, gsl, numpy etc. Also no need for specialized fftw.rb formula since I got upstream to accept building their version with AVX

--- HomebrewFormula/xmds2.rb
+++ HomebrewFormula/xmds2.rb
@@ -5,23 +5,34 @@
 class Xmds2 < Formula
   desc ""
   homepage ""
-  url "https://downloads.sourceforge.net/project/xmds/xmds-2.2.3.tar.gz"
-  sha256 "4363afe894c4eb322d6445ba53f3f124c9cde67937518d0b402f72fc82352924"
+  url "https://downloads.sourceforge.net/project/xmds/xmds-2.3.0-rc1.tar.gz"
+  sha256 "0a0f64ef6f12f24e8f2622e82315064d1ba8fbd9abf8606ac63bf5aadb998bd6"
 
   # depends_on "cmake" => :build
+  depends_on "python"
   depends_on "open-mpi"
+  depends_on "libomp"
   depends_on "hdf5"
+  depends_on "gsl"
   depends_on "fftw" => "with-mpi"
-
-  #resource "Cheetah3" do
-  #  url "https://pypi.python.org/packages/5b/22/0238132792f3600717fad9647072486e2c15b7b45f845b52cc484b9af024/Cheetah3-3.0.0.tar.gz#md5=a1ec55881d2ceb58d6cc985d19ebf745"
-  #  sha256 "53be5db7419e08d71b5b10c9cf0a722c4fd2f5f3aff63976fc22bd20db29f877"
-  #end
   
-  resource "Cheetah" do
-    url "https://pypi.python.org/packages/cd/b0/c2d700252fc251e91c08639ff41a8a5203b627f4e0a2ae18a6b662ab32ea/Cheetah-2.4.4.tar.gz#md5=853917116e731afbc8c8a43c37e6ddba"
-    sha256 "be308229f0c1e5e5af4f27d7ee06d90bb19e6af3059794e5fd536a6f29a9b550"
+  
+  # Apparently resources are the suggested way to install python libraries for homebrew.
+  # Note that these seem to get installed in a virtual env tied to the xmds2 application, not
+  # system wide. This means, for example, that pyparsing is not available to run the test suite
+  # since that is run with ./run_tests and uses system python 3 (which *is* apparently available
+  # since it is installed with depends_on above, so is now a system wide homebrew package)
+  
+  resource "Cheetah3" do
+    url "https://pypi.python.org/packages/5b/22/0238132792f3600717fad9647072486e2c15b7b45f845b52cc484b9af024/Cheetah3-3.0.0.tar.gz#md5=a1ec55881d2ceb58d6cc985d19ebf745"
+    sha256 "53be5db7419e08d71b5b10c9cf0a722c4fd2f5f3aff63976fc22bd20db29f877"
   end
+
+  # This is Cheetah for python 2. Do not want
+  #resource "Cheetah" do
+  #  url "https://pypi.python.org/packages/cd/b0/c2d700252fc251e91c08639ff41a8a5203b627f4e0a2ae18a6b662ab32ea/Cheetah-2.4.4.tar.gz#md5=853917116e731afbc8c8a43c37e6ddba"
+  #  sha256 "be308229f0c1e5e5af4f27d7ee06d90bb19e6af3059794e5fd536a6f29a9b550"
+  #end
 
   resource "pyparsing" do
     url "https://pypi.python.org/packages/3c/ec/a94f8cf7274ea60b5413df054f82a8980523efd712ec55a59e7c3357cf7c/pyparsing-2.2.0.tar.gz#md5=0214e42d63af850256962b6744c948d9"
@@ -32,6 +43,14 @@ class Xmds2 < Formula
     url "https://pypi.python.org/packages/7a/05/b3d1472885d8dc0606936ea5da0ccb1b4785682e78ab15e34ada24aea8d5/mpmath-1.0.0.tar.gz#md5=998f10cb231af62743212ca80693f1b5"
     sha256 "04d14803b6875fe6d69e6dccea87d5ae5599802e4b1df7997bddd2024001050c"
   end
+
+  # We need numpy for matrix transforms like Hermite-Gauss, Bessel etc
+  resource "numpy" do
+    url "https://pypi.python.org/packages/93/48/956b9dcdddfcedb1705839280e02cbfeb2861ed5d7f59241210530867d5b/numpy-1.16.3.zip"
+    sha256 "78a6f89da87eeb48014ec652a65c4ffde370c036d780a995edaeb121d3625621"
+  end
+
+
 
   include Language::Python::Virtualenv
 
